767c093a95a092636bf8a5ad2acabca9b7253ef8
DrivePersinb <mypersonaludisc1@gmail.com>
2025-05-19 21:46:51 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,126 @@
+export const products = [
+  {
+    id: 1,
+    brand: "Yamaha",
+    name: "Pacifica 112V",
+    type: "Electric Guitar",
+    price: 299,
+    image: "https://m.media-amazon.com/images/I/61W2ar5hUJL._AC_SL1500_.jpg",
+    specs: [
+      { label: "Body", value: "Alder" },
+      { label: "Neck", value: "Maple" },
+      { label: "Frets", value: "22" },
+    ],
+    buyLinks: [
+      { site: "Amazon", url: "https://amazon.com/linkexample_ndj" },
+      { site: "Flipkart", url: "https://flipkart.com/linkexample_ndj" }
+    ]
+  },
+  {
+    id: 2,
+    brand: "Roland",
+    name: "TD-1DMK",
+    type: "Electronic Drum Kit",
+    price: 599,
+    image: "https://m.media-amazon.com/images/I/81m3Qd8DTYL._AC_SL1500_.jpg",
+    specs: [
+      { label: "Pads", value: "Dual-mesh" },
+      { label: "Kits", value: "15" },
+      { label: "USB MIDI", value: "Yes" },
+    ],
+    buyLinks: [
+      { site: "Amazon", url: "https://amazon.com/linkexample_roland" }
+    ]
+  },
+  {
+    id: 3,
+    brand: "Fender",
+    name: "Player Jazz Bass",
+    type: "Electric Bass",
+    price: 799,
+    image: "https://m.media-amazon.com/images/I/71TnFQ1dZHL._AC_SL1500_.jpg",
+    specs: [
+      { label: "Body", value: "Alder" },
+      { label: "Pickups", value: "Player Series Alnico 5 Single-Coil" },
+      { label: "Frets", value: "20" },
+    ],
+    buyLinks: []
+  },
+  {
+    id: 4,
+    brand: "Casio",
+    name: "CT-S300",
+    type: "Keyboard",
+    price: 179,
+    image: "https://m.media-amazon.com/images/I/81bD5jFNNpL._AC_SL1500_.jpg",
+    specs: [
+      { label: "Keys", value: "61" },
+      { label: "Polyphony", value: "48 notes" },
+      { label: "MIDI", value: "USB" },
+    ],
+    buyLinks: []
+  },
+  {
+    id: 5,
+    brand: "Shure",
+    name: "SM58",
+    type: "Microphone",
+    price: 99,
+    image: "https://m.media-amazon.com/images/I/61B3zqQF1gL._AC_SL1500_.jpg",
+    specs: [
+      { label: "Type", value: "Dynamic" },
+      { label: "Pattern", value: "Cardioid" },
+      { label: "Connector", value: "XLR" },
+    ],
+    buyLinks: []
+  },
+  // --- New Examples Below ---
+  {
+    id: 6,
+    brand: "Casio",
+    name: "CTK 7300IN",
+    type: "Keyboard",
+    price: 250,
+    image: "https://m.media-amazon.com/images/I/71nR2U8u0PL._AC_SL1500_.jpg",
+    specs: [
+      { label: "Keys", value: "61 (Piano Style)" },
+      { label: "Polyphony", value: "48 Notes" },
+      { label: "Tones", value: "820" },
+      { label: "Rhythms", value: "250" },
+      { label: "Indian Tones", value: "20" },
+      { label: "Indian Rhythms", value: "12" },
+      { label: "Song Sequencer", value: "Yes, 16 tracks" },
+      { label: "Display", value: "Large LCD" },
+      { label: "Speakers", value: "6W + 6W" }
+    ],
+    buyLinks: [
+      { site: "Amazon", url: "https://amazon.com/linkexample_ctk7300in" },
+      { site: "Flipkart", url: "https://flipkart.com/linkexample_ctk7300in" },
+      { site: "Meesho", url: "https://meesho.com/linkexample_ctk7300in" }
+    ]
+  },
+  {
+    id: 7,
+    brand: "Casio",
+    name: "CTX 8000IN",
+    type: "Keyboard",
+    price: 450,
+    image: "https://m.media-amazon.com/images/I/71Kk6m4niML._AC_SL1500_.jpg",
+    specs: [
+      { label: "Keys", value: "61 (Touch Sensitive)" },
+      { label: "Polyphony", value: "128 Notes" },
+      { label: "Tones", value: "800" },
+      { label: "Rhythms", value: "235" },
+      { label: "Indian Tones", value: "43" },
+      { label: "Indian Rhythms", value: "39" },
+      { label: "Song Sequencer", value: "Yes, 17 tracks" },
+      { label: "Display", value: "Backlit LCD" },
+      { label: "Speakers", value: "15W + 15W" }
+    ],
+    buyLinks: [
+      { site: "Amazon", url: "https://amazon.com/linkexample_ctx8000in" },
+      { site: "Flipkart", url: "https://flipkart.com/linkexample_ctx8000in" },
+      { site: "Meesho", url: "https://meesho.com/linkexample_ctx8000in" }
+    ]
+  }
+];
